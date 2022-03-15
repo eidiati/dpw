ab2446fe82924c38e0c9deb462d1f8872564eb7e
unknown <aditsptra27@gmail.con>
2022-03-15 17:25:59 +0700

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,25 @@
+/*a=1;
+var a;
+a=2;
+
+let a=5;
+
+const a;
+console.log(a)
+
+if(a=3)console.log("benar")
+else console.log("salah");
+console.log(typeof a)*/
+const h1=document.getElementById("hi")
+h1.style.color="green";
+h1.style.backgroundColor="purple";
+
+// const h2=document.getElementsByTagName("h1")
+// h2[0].style.color="blue";
+
+// //const h3=document.getElementsByClassName
+
+// const h4=document.querySelector(.coba)
+
+
+document.write("kau ")
